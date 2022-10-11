047cf5d6d371cb4802057a8d9d585939ccb6eff9
Victor Miranda <victormiranda13v@gmail.com>
2022-10-11 10:01:40 -0300

    feat: add some components

--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -2,10 +2,10 @@ import {TextInput} from 'react-native';
 import styled from 'styled-components';
 
 export const Input = styled(TextInput)`
+  padding: 8px;
+  border-radius: 8px;
   color: white;
   background-color: brown;
   width: 100%;
   min-width: 200px;
-  padding: 8px;
-  border-radius: 8px;
 `;
